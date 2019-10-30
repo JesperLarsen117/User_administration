df73745fac32fb8068abe42dbd311c5a1756675a
Kim Ne0 <kimvneve@hotmail.com>
2019-10-30 10:44:30 +0100

    ...

--- delete_user.php
+++ delete_user.php
@@ -8,7 +8,7 @@ include 'content/functions/delete_user_data.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Delete</title>
     <link rel="stylesheet" href="content/css/main.css">
     <link rel="stylesheet" href="content/css/delete_user.css">
 </head>
