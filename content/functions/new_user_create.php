2aa45b94cd1eb581bc6acb13634d5ee9d7c707de
RasmusLasborg <Rasmuslasborg95@gmail.com>
2019-10-31 08:42:04 +0100

    final version

---
+++ content/functions/new_user_create.php
@@ -0,0 +1,82 @@
+<?php
+include '../includes/db.php';
+ini_set ('diplay_error', 1);
+error_reporting(E_ALL);
+// //Our MySQL user account.
+// define('MYSQL_USER', 'root');
+ 
+// //Our MySQL password.
+// define('MYSQL_PASSWORD', '');
+ 
+// //The server that MySQL is located on.
+// define('MYSQL_HOST', 'localhost');
+
+// //The name of our database.
+// define('MYSQL_DATABASE', 'skole');
+
+$host = 'localhost';
+$dbname = 'skole';
+$user = 'root';
+$pass = 'root';
+
+$pdoOptions = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+// $pdo = new PDO(
+//     "mysql:host=" . MYSQL_HOST . "port=1025;dbname=" . MYSQL_DATABASE,
+//     MYSQL_USER, //Username
+//     MYSQL_PASSWORD, //Password
+//     $pdoOptions //Options
+// );
+
+try {
+    $pdo = new PDO("mysql:host={$host};dbname={$dbname}", $user, $pass, $pdoOptions);
+
+} catch (PDOExeption $e) {
+    echo "fejl ".$e->getMessage();
+}
+
+$message = '';
+
+//tjekker om knappen er blevet trykket.
+if (isset($_POST['submit'])) {
+
+    // henter data fra formen, og laver en variable med dens værdi.
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+
+    // query string med named placeholders.
+    $check = "SELECT username FROM users WHERE username = :username";
+    // sikkerhed. prepared statement.
+    $checkStmt = $pdo->prepare($check);
+    // Binder $username til :username
+    $checkStmt->bindValue(':username', $username);
+    // Udfører handlingerne oven over.
+    $checkStmt->execute();
+        // Checker om der er  en fundet et username på databsen, ud fra hvad man skriver i formen.
+        if (!$checkStmt->rowCount() > 0 )
+        {
+            // query string med named placeholders.
+            $sql = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+            // sikkerhed. prepared statement.
+            $stmt = $pdo->prepare($sql);
+            // sikkerhed. prepared statement.
+            $stmt->bindValue(':username', $username);
+            // sikkerhed. prepared statement.
+            $stmt->bindValue(':email', $email);
+            // sikkerhed. prepared statement.
+            $stmt->bindValue(':password', $password);
+            // Udfører handlingerne oven over.
+            $stmt->execute();
+
+            header('Location: ../../index.php');
+        } else {
+            // header('Location: ../../new_user.php');
+            $message = 'Brugeren findes!';
+        }
+}
+
+?>
--- new_user.php
+++ new_user.php
@@ -1,5 +1,5 @@
 <?php
-include 'content/functions/send_user_data.php';
+include 'content/functions/new_user_create.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,11 +7,21 @@ include 'content/functions/send_user_data.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="content/css/main.css">
+    <title>New User</title>
     <link rel="stylesheet" href="content/css/new_user.css">
 </head>
 <body>
-    
+
+
+
+    <h1>Create new user</h1>
+<form method="post">
+    <input type="text" name="username" id="name" placeholder="Username"><br>
+    <input type="email" name="email" id="email" placeholder="E-mail"><br>
+    <input type="password" name="pass" id="pass" placeholder="Password"><br>
+    <?php echo $message; ?>
+    <input class ="create" type="submit" value="Create" name="submit">
+    <a class="cancel" href="index.php">Cancel</a>
+</form>
 </body>
 </html>
